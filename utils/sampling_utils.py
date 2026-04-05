6e5e7896082ca72bbceba4090048accdbbf459ca
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2026-04-05 09:21:45 +0300

    produce RGB images

--- utils/sampling_utils.py
+++ utils/sampling_utils.py
@@ -18,15 +18,15 @@ def sample_and_save(fm, num_of_images, device, epoch, save_dir, num_of_ch, steps
 
 def save_one_step_sample(k, path, name, samples_one_step):
     """Save generated samples as images"""
-    p = 'cifar' in path or 'celeb' in path
+    is_rgb = samples_one_step.shape[1] == 3
     samples_one_step = torch.clamp(samples_one_step, 0, 1)
-    cmap = None if p else 'gray'
+    cmap = None if is_rgb else 'gray'
 
     grid_size = int(k ** 0.5)
     fig, axes = plt.subplots(grid_size, grid_size, figsize=(8, 8))
     for i, ax in enumerate(axes.flat):
         if i < k:
-            img = samples_one_step[i, :].cpu().permute(1, 2, 0).squeeze().numpy() if p else samples_one_step[
+            img = samples_one_step[i, :].cpu().permute(1, 2, 0).numpy() if is_rgb else samples_one_step[
                 i, 0].cpu().numpy()
             ax.imshow(img, cmap=cmap)
         ax.axis('off')
